3971ef70cbc0be54a1e4fab18e17ca030f9f66d8
Yannic Labartino <yannic.labartino@icloud.com>
2021-03-17 11:28:04 +0100

    fix merge

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -7,59 +7,64 @@ import {HeroService} from '../services/hero.service';
 import {Guild} from '../data/guild';
 
 @Component({
-    selector: 'app-hero-detail',
-    templateUrl: './hero-detail.component.html',
-    styleUrls: ['../app.component.css', './hero-detail.component.css']
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['../app.component.css', './hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
 
-    @Input() hero: Hero | undefined;
-    messageError: string;
-    pointsToDistribute: number;
-    readonly maxPoints: number = 40;
-    guilds: Guild[] = [];
+  @Input() hero: Hero | undefined;
+  messageError: string;
+  pointsToDistribute: number;
+  readonly maxPoints: number = 40;
+  guilds: Guild[] = [];
 
-    constructor(private route: ActivatedRoute,
-                private heroService: HeroService,
-                private location: Location,
-                private router: Router) {
+  constructor(private route: ActivatedRoute,
+              private heroService: HeroService,
+              private location: Location,
+              private router: Router) {
+  }
+
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id.toString())
+      .subscribe(hero => this.hero = hero);
+  }
+
+  getGuilds(): void {
+    this.heroService.getGuilds()
+      .subscribe(guild => this.guilds = guild);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  applyChanges(): void {
+    if (this.addAllPoints() > this.maxPoints) {
+      this.messageError = "Vous avez dépassé les " + this.maxPoints + " points à répartir";
+    } else {
+      this.messageError = null;
+      this.heroService.updateHero(this.hero);
+      this.router.navigate(['heroes']);
     }
-
-
-    ngOnInit(): void {
-        this.getHero();
-    }
-
-    getHero(): void {
-        const id = +this.route.snapshot.paramMap.get('id');
-        this.heroService.getHero(id.toString())
-            .subscribe(hero => this.hero = hero);
-    }
-
-    goBack(): void {
-        this.location.back();
-    }
-
-    applyChanges(): void {
-        if ( this.addAllPoints() > this.maxPoints) {
-            this.messageError = "Vous avez dépassé les "+ this.maxPoints +" points à répartir";
-        } else {
-            this.messageError = null;
-            this.heroService.updateHero(this.hero);
-            this.router.navigate(['heroes']);
-        }
-    }
+  }
 
   changeGuild(num): void {
     this.hero.guild = Object.assign({}, this.guilds[num - 1]);
   }
 
-    updatePoints(): void {
-      this.pointsToDistribute = this.maxPoints - this.addAllPoints();
-    }
+  updatePoints(): void {
+    this.pointsToDistribute = this.maxPoints - this.addAllPoints();
+  }
 
-    addAllPoints(): number{
-      return (this.hero.health + this.hero.attack + this.hero.damage + this.hero.dodge);
-    }
+  addAllPoints(): number {
+    return (this.hero.health + this.hero.attack + this.hero.damage + this.hero.dodge);
+  }
 
 }
