e64442c23c4d609ede2eece1c851b6f28cd4329c
Yannic Labartino <yannic.labartino@icloud.com>
2021-05-03 20:16:59 +0200

    default images

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -22,6 +22,7 @@ export class HeroDetailComponent implements OnInit {
   defaultsName: string[] = ["Juan", "Paolo", "Hinata",
     "Laure", "Margot", "Penelope", "Kageyama", "Jocelyne", "Alfred", "Jerôme"
     , "Viviane"];
+  create: boolean;
 
   constructor(private route: ActivatedRoute,
               private heroService: HeroService,
@@ -31,69 +32,79 @@ export class HeroDetailComponent implements OnInit {
   }
 
 
-    ngOnInit(): void {
-        this.getHero();
-    }
+  ngOnInit(): void {
+    this.getHero();
+    this.create = this.route.snapshot.url[1].path === 'create';
+  }
 
-    getHero(): void {
-        const id = +this.route.snapshot.paramMap.get('id');
-        if (id !== 0){
-          this.heroService.getHero(id.toString())
-            .subscribe(hero => {
-              this.hero = hero;
-              this.updatePoints();
-              this.getGuilds();
-            });
-        }else{
-          this.getGuilds();
-          this.hero = new Hero();
-          this.hero.name = this.defaultsName[Math.round(Math.random() * (this.defaultsName.length - 1))];
-          this.hero.description = 'Default description';
-          this.hero.attack = Math.round(Math.random() * 10);
-          this.hero.dodge = Math.round(Math.random() * 10);
-          this.hero.damage = Math.round(Math.random() * 10);
-          this.hero.health = Math.round(Math.random() * 10);
-          this.hero.victories = 0;
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    if (id !== 0) {
+      this.heroService.getHero(id.toString())
+        .subscribe(hero => {
+          this.hero = hero;
           this.updatePoints();
-        }
+          this.getGuilds();
+        });
+    } else {
+      this.getGuilds();
+      this.hero = new Hero();
+      this.hero.name = this.defaultsName[Math.round(Math.random() * (this.defaultsName.length - 1))];
+      this.hero.description = 'Insert your best description';
+      this.hero.attack = Math.round(Math.random() * 10);
+      this.hero.dodge = Math.round(Math.random() * 10);
+      this.hero.damage = Math.round(Math.random() * 10);
+      this.hero.health = Math.round(Math.random() * 10);
+      this.hero.victories = 0;
+      console.log(this.hero.guild);
+      this.updatePoints();
     }
+  }
 
-    goBack(): void {
-        this.location.back();
-    }
+  getImage(): void {
+    console.log('changing image');
+    this.hero.image = this.hero.guild.name.toLowerCase() + '.png';
+  }
 
-    applyChanges(): void {
-        if ( this.addAllPoints() > this.maxPoints) {
-            this.messageError = "Vous avez dépassé les "+ this.maxPoints +" points à répartir";
-        } else {
-            this.messageError = null;
-            if (this.hero.id !== undefined){
-              this.heroService.updateHero(this.hero);
-              this.router.navigate(['heroes']);
-            }else{
-                this.heroService.getLastId().subscribe(
-                  id => {
-                    this.hero.id = id;
-                    this.heroService.addHero(this.hero).then(
-                      result => {
-                        this.router.navigate(['detail/' + this.hero.id]);
-                      }
-                    );
-                  }
-                );
-            }
-        }
+  goBack(): void {
+    this.location.back();
+  }
+
+  applyChanges(): void {
+    if (this.addAllPoints() > this.maxPoints) {
+      this.messageError = "Vous avez dépassé les " + this.maxPoints + " points à répartir";
+    } else {
+      this.messageError = null;
+      if (this.hero.id !== undefined) {
+        this.heroService.updateHero(this.hero);
+        this.router.navigate(['heroes']);
+      } else {
+        this.heroService.getLastId().subscribe(
+          id => {
+            this.hero.id = id;
+            this.heroService.addHero(this.hero).then(
+              result => {
+                this.router.navigate(['detail/' + this.hero.id]);
+              }
+            );
+          }
+        );
+      }
     }
+  }
 
   changeGuild(num): void {
     this.hero.guild = Object.assign({}, this.guilds[num - 1]);
+    if (this.create) {
+      this.getImage();
+    }
   }
 
   updatePoints(): void {
     this.pointsToDistribute = this.maxPoints - this.addAllPoints();
   }
 
-  addAllPoints(): number{
+  addAllPoints(): number {
     return (this.hero.health + this.hero.attack + this.hero.damage + this.hero.dodge);
   }
 
@@ -101,9 +112,10 @@ export class HeroDetailComponent implements OnInit {
     this.guildService.getGuilds()
       .subscribe(guilds => {
         this.guilds = guilds;
-        if (this.hero.guild === undefined){
-          // console.log(this.guilds.length);
-          this.hero.guild = Object.assign({}, this.guilds[Math.round(Math.random() * (this.guilds.length - 1))]);
+        if (this.hero.guild === undefined) {
+
+          const guildID = Math.round(Math.random() * (this.guilds.length - 1)) + 1;
+          this.changeGuild(guildID);
         }
       });
   }
